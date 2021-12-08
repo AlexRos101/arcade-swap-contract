0746364715914a0dd0476413e2b6e32df54a2822
deep-quality-dev <hungrywarrior081@gmail.com>
2021-12-08 22:31:30 +0800

    fix: output abi in data folder

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("@openzeppelin/hardhat-upgrades");
 require("hardhat-gas-reporter");
+require("hardhat-abi-exporter");
 require("solidity-coverage");
 
 // This is a sample Hardhat task. To learn how to create your own go to
